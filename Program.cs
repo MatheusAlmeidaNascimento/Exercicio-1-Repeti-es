319366ecec8bad3aeadd00ccd3cc911f5b1f2d7d
MatheusAlmeidaNascimento <86276081+MatheusAlmeidaNascimento@users.noreply.github.com>
2021-06-25 22:55:02 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio_1_Repetições
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           bool respostaCorreta = false;
+
+           while (respostaCorreta == false)
+           {
+               Console.WriteLine ("Escolha uma nota de 0 a 10");
+               int resposta = int.Parse(Console.ReadLine());
+
+               if (resposta < 11)
+               {
+                  respostaCorreta = true; 
+               }
+               else if (resposta >= 11)
+               {
+                   Console.WriteLine("Esse não é um valor válido");
+                   Console.WriteLine("Por favor, informe um valor válido");
+
+               }
+
+           } 
+                Console.WriteLine("Esse é um valor válido");
+            
+        }
+    }
+}
+
+        
+    
+
